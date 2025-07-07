ebfe622c562c55ed1a2b8a12857899233ea2869e
HellGa <esfero1307@gmail.com>
2025-07-07 18:10:23 +0300

    maybe last

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -5,7 +5,7 @@ import {
     getContactByIdController, getContactsController,
     patchContactController,
 } from '../controllers/contacts.js';
-import { ctrlWrapper } from '../utils/ctrlWrapper';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 
 const router = Router();
