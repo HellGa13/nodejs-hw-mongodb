5abb2b3204fa3aad13fd7c4bb21297b5aa6a4bc6
HellGa <esfero1307@gmail.com>
2025-07-14 20:15:37 +0300

    done second step

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -6,15 +6,19 @@ import {
     patchContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { createContactSchema } from '../validation/contacts.js';
+import { updateContactSchema } from '../validation/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
-router.post('/contacts', ctrlWrapper(createContactsController));
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
-router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+router.get('/contacts/:contactId', isValidId, ctrlWrapper(getContactByIdController));
+router.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createContactsController));
+router.delete('/contacts/:contactId', isValidId, ctrlWrapper(deleteContactController));
+router.patch('/contacts/:contactId', validateBody(updateContactSchema), ctrlWrapper(patchContactController));
 
 
 export default router;
