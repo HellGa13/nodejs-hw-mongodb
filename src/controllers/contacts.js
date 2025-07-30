e4d1fe3f7ab26af322dd3e72eb09ddeb3805f15c
HellGa <esfero1307@gmail.com>
2025-07-30 12:37:22 +0300

    done 5

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -72,7 +72,7 @@ export const deleteContactController = async (req, res, next) => {
 
 export const patchContactController = async (req, res, next) => {
 const { contactId } = req.params;
-  const result = await updateContact(contactId, req.user._id, req.body );
+  const result = await updateContact(contactId, req.body, req.user._id );
 
   if (!result) {
     next(createHttpError(404, 'Contact not found'));
