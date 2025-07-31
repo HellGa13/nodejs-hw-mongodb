141ede1044387ce2659ccd2daf52a8d2a0739a5d
HellGa <esfero1307@gmail.com>
2025-07-31 11:33:57 +0300

    create new branch + done resert password

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -2,13 +2,15 @@ import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
 
-import { registerUserSchema } from '../validation/auth.js';
-import { registerUserController } from '../controllers/auth.js';
-
-import { loginUserSchema} from '../validation/auth.js';
-import { loginUserController } from '../controllers/auth.js';
-import { logoutUserController } from '../controllers/auth.js';
-import { refreshUserSessionController } from '../controllers/auth.js';
+import {
+    registerUserSchema, loginUserSchema,
+    requestResetEmailSchema, resetPasswordSchema
+} from '../validation/auth.js';
+import {
+    registerUserController, loginUserController,
+    logoutUserController, refreshUserSessionController,
+    requestResetEmailController, resetPasswordController
+} from '../controllers/auth.js';
 
 const router = Router();
 
@@ -16,5 +18,7 @@ router.post('/register', validateBody(registerUserSchema), ctrlWrapper(registerU
 router.post('/login', validateBody(loginUserSchema), ctrlWrapper(loginUserController));
 router.post('/logout', ctrlWrapper(logoutUserController));
 router.post('/refresh', ctrlWrapper(refreshUserSessionController));
+router.post('/request-reset-email', validateBody(requestResetEmailSchema), ctrlWrapper(requestResetEmailController));
+router.post('/reset-password', validateBody(resetPasswordSchema), ctrlWrapper(resetPasswordController));
 
 export default router;
